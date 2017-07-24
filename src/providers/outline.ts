2c79de12d10ace70a27ba7926645151b323a7167
Justin Coulston <justin.coulston@gmail.com>
2017-07-24 00:42:22 -0400

    Organized outline by types of defintions

--- src/providers/outline.ts
+++ src/providers/outline.ts
@@ -44,7 +44,7 @@ export class SectionNodeProvider implements vscode.TreeDataProvider<Section> {
 
         // Establish patterns for the searches
         const basicPattern = /^[ \t]*\[([\w\d_,\s]+)\]/gm
-
+        const basicTypesSection = new Section('Basic Types');
         while(true){
             const result = basicPattern.exec(content);
             if (!result) break;
@@ -55,23 +55,33 @@ export class SectionNodeProvider implements vscode.TreeDataProvider<Section> {
 
             // comma-separated array for basic definitions in Z
             for(let s of splits){
-                const newSection = new Section(s.trim(), vscode.TreeItemCollapsibleState.Collapsed, lineNumber, filePath);
-                sections.push(newSection);
+                const newSection = new Section(s.trim(), lineNumber, filePath);
+                basicTypesSection.children.push(newSection);
             }
+        }
+        if(basicTypesSection.children.length > 0) {
+            basicTypesSection.children.sort((n1, n2) => n1.label.localeCompare(n2.label))
+            sections.push(basicTypesSection);
         }
 
         // Get Schema Definitions
         const schemaPattern = /^[ \t]*\\begin\{schema\}\{([\w\d_ ,\[\]]+)\}/gmi
+        const schemaSection = new Section('Schemas')
         while(true){
             const result = schemaPattern.exec(content);
             if (!result) break;
             const lineNumber = this.findLineNumber(content, result.index);
             const label = result[1];
-            const newSection = new Section(label, vscode.TreeItemCollapsibleState.Collapsed, lineNumber, filePath);
-            sections.push(newSection);
+            const newSection = new Section(label, lineNumber, filePath);
+            schemaSection.children.push(newSection);
+        }
+        if (schemaSection.children.length > 0) {
+            schemaSection.children.sort((n1, n2) => n1.label.localeCompare(n2.label))
+            sections.push(schemaSection);
         }
 
         const axiomPattern = /^[ \t]*\\begin\{axdef\}([\s\S]+?)(\\end\{axdef\})/gmi
+        const axiomSection = new Section('Axioms')
         while(true){
             const result = axiomPattern.exec(content);
             if (!result) break;
@@ -86,47 +96,69 @@ export class SectionNodeProvider implements vscode.TreeDataProvider<Section> {
                 const labels = innerResult[1];
                 const splits = labels.split(',');
                 for(let s of splits){
-                    const newSection = new Section(s.trim(), vscode.TreeItemCollapsibleState.Collapsed, lineNumber, filePath)
-                    sections.push(newSection);
+                    const newSection = new Section(s.trim(), lineNumber, filePath)
+                    axiomSection.children.push(newSection);
                 }
-                
             }
+        }
+        if (axiomSection.children.length > 0){
+            axiomSection.children.sort((n1, n2) => n1.label.localeCompare(n2.label))
+            sections.push(axiomSection);
         }
 
         const genPattern = /^[ \t]*\\begin\{gendef\}\s*?(\[[\w\d ,_]+\])/gmi
+        const genSection = new Section('Generics')
         while(true){
             const result = genPattern.exec(content);
             if (!result) break;
 
             const lineNumber = this.findLineNumber(content, result.index);
             const label = result[1];
-            const newSection = new Section(label.trim(), vscode.TreeItemCollapsibleState.Collapsed, lineNumber, filePath)
-            sections.push(newSection);
+            const newSection = new Section(label.trim(), lineNumber, filePath)
+            genSection.children.push(newSection);
+        }
+        if (genSection.children.length > 0){
+            genSection.children.sort((n1, n2) => n1.label.localeCompare(n2.label));
+            sections.push(genSection);
         }
 
         const listPattern = /^[ \t]*([\w\d_]+)\s*?::=/gm
+        const listSection = new Section('Lists')
         while(true){
             const result = listPattern.exec(content);
             if (!result) break;
 
             const lineNumber = this.findLineNumber(content, result.index);
             const label = result[1];
-            const newSection = new Section(label.trim(), vscode.TreeItemCollapsibleState.Collapsed, lineNumber, filePath)
-            sections.push(newSection);
+            const newSection = new Section(label.trim(), lineNumber, filePath)
+            listSection.children.push(newSection);
+        }
+        if (listSection.children.length > 0){
+            listSection.children.sort((n1, n2) => n1.label.localeCompare(n2.label))
+            sections.push(listSection);
         }
 
         const inlineSchemaPattern = /^[ \t]*([\w\d_]+)\s*? ==/gm
+        const inlineSchemas = new Section('Schemas (Inline)')
         while(true){
             const result = inlineSchemaPattern.exec(content);
             if (!result) break;
 
             const lineNumber = this.findLineNumber(content, result.index);
             const label = result[1];
-            const newSection = new Section(label.trim(), vscode.TreeItemCollapsibleState.Collapsed, lineNumber, filePath)
-            sections.push(newSection);
+            const newSection = new Section(label.trim(), lineNumber, filePath)
+            inlineSchemas.children.push(newSection);
+        }
+        if (inlineSchemas.children.length > 0){
+            inlineSchemas.children.sort((n1, n2) => n1.label.localeCompare(n2.label))
+            sections.push(inlineSchemas)
         }
 
-        return sections;
+        return sections.sort((n1, n2) => {
+            if (n1.label > n2.label) return 1;
+            if (n1.label < n2.label) return -1;
+            return 0;
+        });
     }
 
     getTreeItem(element: Section) : vscode.TreeItem {
@@ -146,23 +178,24 @@ export class SectionNodeProvider implements vscode.TreeDataProvider<Section> {
 }
 
 class Section extends vscode.TreeItem {
-
     public children: Section[] = []
     public command: vscode.Command
 
     constructor(
         label: string,
-        collapsibleState: vscode.TreeItemCollapsibleState,
-        lineNumber: number,
-        fileName: string
+        lineNumber?: number,
+        fileName?: string
     ) {
-        super(label, collapsibleState)
+        super(label, vscode.TreeItemCollapsibleState.Collapsed)
 
-        this.command = {
-            "command": 'zed-workshop.goto-section',
-            "title": '',
-            "arguments":[fileName, lineNumber]
+        if (fileName !== undefined && lineNumber !== undefined){
+            this.command = {
+                        "command": 'zed-workshop.goto-section',
+                        "title": '',
+                        "arguments":[fileName, lineNumber]
+                    }
         }
+        
     }
 
     iconPath = {
